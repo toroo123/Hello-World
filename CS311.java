ef68ac156e87722b90e877697d875fec13d2a313
toroo123 <tormandah1@gmail.com>
2022-02-25 15:12:42 +0800

    shineer nemsen

---
+++ CS311.java
@@ -0,0 +1,48 @@
+package CS311;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
+
+
+public class lab_1 {
+
+    public static void main(String args[]) {
+       List<String> cities = Arrays.asList("London", "HongKong", 
+                                          "Paris", "NewYork");
+       System.out.println("Original list : " + cities);
+       System.out.println("list transformed using Java 8 :" 
+                                   + transform(cities));
+       System.out.println("list transformed using loop before Java 8 : " 
+                                   + beforeJava8(cities));
+       
+       
+       
+       List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+       List<Integer> squares = numbers.stream()
+                                      .map( i -> i*i)
+                                      .collect(Collectors.toList());
+       System.out.println("original list of numbers : " + numbers);
+       System.out.println("transformed list of integers using Map in Java 8 : "
+                                   + squares);
+
+    }
+
+    
+    public static List<String> beforeJava8(List<String> listOfString) {
+        List<String> coll = new ArrayList<>();
+        for (String str : listOfString) {
+            coll.add(str.toUpperCase());
+        }
+        return coll;
+    }
+
+    
+    
+    public static List<String> transform(List<String> listOfString) {
+        return listOfString.stream().map(String::toUpperCase) .collect(toList());
+    }
+
+}
