220d594f1d127bd1a5e7ed1c60304e8726c32a6c
toroo123 <tormandah1@gmail.com>
2022-03-04 15:17:37 +0800

    kodoo dahiad oorchilson shuu

--- CS311.java
+++ CS311.java
@@ -1,128 +1,110 @@
 package CS311;
-
-import dataStructures.ArrayStack;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.awt.List;
 import java.util.*;
 
-public class MyStack extends ArrayStack {
 
-	public int size() {
-		if(!empty()) return top+1;
-		else return 0;
-	}
-	public Object[] toArray() {
-		Object[] obj = new Object[size()];
-		int i = size() - 1;
-		// to array
-		while(!empty()) {
-			obj[i--] = pop();
-		}
-		// push stack again
-		for(int j = 0; j < obj.length; j++) {
-			push(obj[j]);
-		}
-		return obj;
-	}
-	public MyStack reverse(MyStack stack) {
-		Object[] obj = new Object[stack.size()];
-		
-		int i = 0;
-		while(!stack.empty()) {
-			obj[i++] = stack.pop();
-		}
-		
-		MyStack s = new MyStack();
-		for(int j = 0; j < obj.length; j++) {
-			s.push(obj[j]);
-		}
-		return s;
-	}
+public class CS311 {
+
+	static Scanner sc = new Scanner(System.in);
+	static Queue<Integer> list = new LinkedList<>();
 	
-	public String printStack() {
-		Object[] arr = toArray();
-		boolean first = true;
-		String result = "";
-		for(Object i : arr) {
-			result += "\t" + i + ((first == true)? " -> bottom": "") + "\n";
-			first = false;
-		}
-		result = result.substring(0,result.length() - 1);
-		return result += " -> top";
-	}
-	public MyStack splitStack() {
-		int mid = size() / 2 - 1;
+	public static void max() {
+		Object a = new Object();
+		a= ((LinkedList<Integer>) list).get(0);
 		
-		MyStack stack = new MyStack();
-		for(int i = 0; i <= mid; i++) {
-			stack.push(pop());
-		}
-		return reverse(stack);
-	}
-	
-	public void combineStack(MyStack stack) {
-		Object[] obj = stack.toArray();
-		
-		for( int i = 0; i < obj.length; i++) {
-			push(obj[i]);
-		}
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MyStack mystack = new MyStack();
-		mystack.push(1);
-		mystack.push(2);
-		mystack.push(3);
-		mystack.push(4);
-		mystack.push(5);
-		mystack.push(6);
-		mystack.push(1);
-		mystack.push(5);
-		mystack.push(6);
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("============================================MENU============================================");
-		System.out.println("empty\npeek\npush\npop");
-		System.out.println("\n\nsize\ntoArray");
-		System.out.println("\n\ninputStack\nprintStack\nsplitStack\ncombineStack");
-		for(;;) {
-			System.out.print("\nEnter your command: ");
-			String songolt = sc.next();
-			
-			switch(songolt) {
-			case "empty": System.out.println("Is empty: " + mystack.empty()); break;
-			case "peek": System.out.println("Peek element: " + mystack.peek()); break;
-			case "push": {
-				System.out.print("Enter push element: ");
-				try {
-				mystack.push(sc.nextInt());
-				} 
-				catch(Exception e) {
-					System.out.println("push case error!");
-				}
-				break;
+		for(int i=1; i<list.size(); i++)
+			if((int)a<(int) ((LinkedList<Integer>) list).get(i)) {
+				a = ((LinkedList<Integer>) list).get(i);
 			}
-			case "pop": try {System.out.println("Pop: " + mystack.pop());} catch(Exception e) {System.out.println("pop case error!");} break; 
-			case "size": System.out.println("Size: " + mystack.size()); break;
-			case "toArray": {Object[] array = mystack.toArray(); System.out.print("Array >> "); for(Object i : array) {System.out.print(i + " ");} break;}
-			case "printStack": System.out.println(mystack.printStack()); break;	
-			case "splitStack": 
-				System.out.println("\n\tHuvaaj avsan stack : \n\n" + mystack.splitStack().printStack() + "\n\n\tUndsen Stack : \n\n"
-						 + mystack.printStack()); break;	
-			case "combineStack": 
-				MyStack s = new MyStack(); 
-				s.push(100);
-				s.push(200);
-				s.push(300);
-				System.out.println(mystack.printStack() + "\n\n\t stack deer \n\n" + s.printStack() + "\n\n\t stack-g nemlee \n");
-				mystack.combineStack(s);
-				System.out.println(mystack.printStack()); 
-				break;	
-
-			
-			case "exit": System.exit(0); sc.close(); break;
-			default: System.out.println("Your command is wrong. Enter another command, please ..."); break;
+		System.out.println("max: " + a);
+	}
+	public static void remove(int rem) {
+		for(int i=0;i<list.size();i++) {
+			int a1 = ((LinkedList<Integer>) list).get(i);
+			if(a1 == rem) {
+				boolean removedele = list.remove(a1);
 			}
 		}
+		 
+	        System.out.println(list);
 	}
+	public static void peek() {
+		int pee = list.peek();
+		System.out.println("хамгийн эхний element бол " +pee);
+	}
+	public static void poll() {
+		System.out.println(list.poll());
+	}
+	public static void view() {
+		System.out.println(list);
+	}
+	
+	
+    public static void main(String args[]) {
+    
+    	System.out.println("та тоонуудаа оруулж өгнө үү?");
+    	System.out.println("оруулж дууссан бол 0-ийн тоог өгч дуусгана уу.");
+		 int input = sc.nextInt();
+		 while(input != 0) {
+			 list.add(input);
+			 input = sc.nextInt();
+		 }
+		 
+		 System.out.println("өгнө тоонууд " +list);
 
+		 System.out.println("1. max\n 2. remove\n3. add\n4. peek\n5. poll\n6. view\n");
+		 int output = sc.nextInt();
+		 
+		try {
+			while(output != 0) {
+				int index;
+				 
+				 switch(output) {
+				 case 1:
+					 max();
+					 break;
+				 case 2:
+					 System.out.println("тоогоо оруулна уу?");
+					 int rv = sc.nextInt();
+					 remove(rv);
+					 break;
+				 case 3:
+					 System.out.println("та хэдэн тоо оруулах вэ?");
+					 int a = sc.nextInt();
+					 System.out.println("тоонуудаа оруулна уу?");
+					 for(int i=0;i<a;i++) {
+						 int adds = sc.nextInt();
+						 list.add(adds);
+					 }
+					 System.out.println(list);
+					 break;
+				 case 4:
+					 peek();
+					 break;
+				 case 5:
+					 poll();
+					 break;
+				 case 6:
+					 view();
+					 break;
+				 case 9: 
+					 System.out.println("сонголтууд:\n" + "1.max\n2. remove\n3. add\n4. peek\n5. poll\n6. view\n");
+					 System.out.println("\n таны сонголт: ");
+				 default:
+				 }
+				 System.out.println("сонголтуудыг хархаар бол 9-ийг өгнө үү?");
+				 System.out.println("программыг дуусгах бол 0-ийг өгнө үү?");
+                 output = sc.nextInt();
+			}
+            System.out.println("System ends:");
+            sc.close();
+		}catch(Exception e) {
+            System.out.println("input aldaa garlaa"+e);
+            main(args);
+        }
+		  
+    }
 }
