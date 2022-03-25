228f163ef3f755734d7af0f3f7c346a989fd0570
toroo123 <tormandah1@gmail.com>
2022-03-25 15:15:06 +0800

    15:00

--- lab_1.java
+++ lab_1.java
@@ -1,85 +1,52 @@
-package CS311;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.awt.List;
 import java.util.*;
 
+public class lab_1 {
 
-class Queue<T> {
-
-	int front = -1, rear = -1;
-	private LinkedList<T> list = new LinkedList<T>();
-    ArrayList<T> A = new ArrayList<>();
- 
-    T front()
-    {
-        if (front == -1)
- 
-            return null;
-        return A.get(front);
-    }
-    T rear()
-    {
-        if (rear == -1)
-            return null;
-        return A.get(rear);
-    }
-    void enqueue(T X)
-    {
-        if (this.empty()) {
-            front = 0;
-            rear = 0;
-            A.add(X);
-        }
- 
-        else {
-            front++;
-            if (A.size() > front) {
- 
-                A.set(front, X);
-            }
-            else
- 
-                A.add(X);
-        }
-    }
+	static Scanner sc = new Scanner(System.in);
+	static Queue<Integer> list = new LinkedList<>();
 	
-	private boolean empty() {
-		if (front == -1 && rear == -1)
-            return true;
-		return false;
+	public static void max() {
+		Object a = new Object();
+		a= ((LinkedList<Integer>) list).get(0);
+		
+		for(int i=1; i<list.size(); i++)
+			if((int)a<(int) ((LinkedList<Integer>) list).get(i)) {
+				a = ((LinkedList<Integer>) list).get(i);
+			}
+		System.out.println("max: " + a);
 	}
-	public int size() {
-		ArrayList<T> list = A;
-		return list.size();
+	public static void remove() {
+				int removedele = list.remove();
+				System.out.println(list);
 	}
-	public boolean hasItems() {
-	    return !A.isEmpty();
-	  }
-
-//	public static void peek() {
-//		int pee = list.peek();
-//		System.out.println("хамгийн эхний element бол " +pee);
-//	}
-
-//		public static int view() {
-//		return list1;
-//	}
-		public T poll() {
-			T a = ((Deque<T>) A).poll();
-			return a;
-		}
-}
-public class lab_1{
+	public static void peek() {
+		int pee = list.peek();
+		System.out.println("хамгийн эхний element бол " +pee);
+	}
+	public static void poll() {
+		System.out.println(list.poll());
+	}
+	public static void view() {
+		System.out.println(list);
+	}
+	
 	
     public static void main(String args[]) {
     
-    	Scanner sc = new Scanner(System.in);
-    	Queue<Integer> list = new Queue<>();
+    	System.out.println("та тоонуудаа оруулж өгнө үү?");
+    	System.out.println("оруулж дууссан бол 0-ийн тоог өгч дуусгана уу.");
+		 int input = sc.nextInt();
+		 while(input != 0) {
+			 list.add(input);
+			 input = sc.nextInt();
+		 }
 		 
-		 list.enqueue(7);
-		 list.enqueue(6);
-		 list.enqueue(5);
-		 list.enqueue(4);
-		 list.enqueue(3);
 		 System.out.println("өгнө тоонууд " +list);
+
 		 System.out.println("1. max\n 2. remove\n3. add\n4. peek\n5. poll\n6. view\n");
 		 int output = sc.nextInt();
 		 
@@ -89,40 +56,39 @@ public class lab_1{
 				 
 				 switch(output) {
 				 case 1:
-					 System.out.println(list.size());
+					 max();
 					 break;
 				 case 2:
-					 System.out.println(list.hasItems());
+					 remove();
 					 break;
 				 case 3:
 					 System.out.println("та хэдийн тоо оруулах вэ?");
 					 int a = sc.nextInt();
-						 list.enqueue(a);
+						 list.add(a);
 					 System.out.println(list);
 					 break;
 				 case 4:
-//					 
+					 peek();
 					 break;
 				 case 5:
-					 System.out.println(list.poll()); 
+					 poll();
 					 break;
 				 case 6:
-//					 System.out.println(list.view()); 
+					 view();
 					 break;
 				 case 9: 
 					 System.out.println("сонголтууд:\n" + "1.max\n2. remove\n3. add\n4. peek\n5. poll\n6. view\n");
-					 System.out.println("\n таны сонголт: !!");
 					 System.out.println("\n таны сонголт: !xaxa");
 				 default:
 				 }
-				 System.out.println("сонголтуудыг сонгохоор бол 9-ийг өгнө үү?");
+				 System.out.println("сонголтуудыг хархаар бол 9-ийг өгнө үү?");
 				 System.out.println("программыг дуусгах бол 0-ийг өгнө үү?");
                  output = sc.nextInt();
 			}
             System.out.println("System ends:");
             sc.close();
 		}catch(Exception e) {
-            System.out.println("oroltond aldaa garlaa"+e);
+            System.out.println("input aldaa garlaa"+e);
             main(args);
         }
 		  
